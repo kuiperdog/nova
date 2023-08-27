a0555a8da068f529b899ddd96eb611377683e570
kuiper <kuiper@kuiper.dog>
2023-08-26 20:35:52 -0700

    Define API endpoints globally

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 
 const app = createApp(App)
 
+app.config.globalProperties.SUBSQUID_API = 'https://squid.subsquid.io/cosmo/v/v1/graphql'
+app.config.globalProperties.COSMO_API = 'https://api.cosmo.fans'
+
 app.use(router)
 
 app.mount('#app')
