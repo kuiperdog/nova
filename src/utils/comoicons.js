4fbfbd72cf440079b29f322a04a7ef005a71bbfb
kuiper <kuiper@kuiper.dog>
2023-11-30 12:31:33 -0800

    Init version 1.0.0

--- src/utils/comoicons.js
+++
@@ -1,9 +0,0 @@
-import tripleS_como from '@/assets/images/tripleS-como.svg'
-import artms_como from '@/assets/images/artms-como.svg'
-
-const comoIcons = {
-    'tripleS': tripleS_como,
-    'artms': artms_como
-}
-
-export default comoIcons
