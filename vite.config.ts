5eebc3fba6a3fb8744d3f028769f4debc249d235
kuiper <kuiper@kuiper.dog>
2024-05-19 15:11:17 -0700

    Pre-transform models with tsc for decorator metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { vitePluginTypescriptTransform } from 'vite-plugin-typescript-transform';
 import pkg from './package.json' assert { type: 'json' };
 import path from 'path';
 
 export default defineConfig({
     plugins: [
         sveltekit(),
-        nodePolyfills()
+        nodePolyfills(),
+        vitePluginTypescriptTransform({
+            enforce: 'pre',
+            filter: {
+                files: {
+                    include: /\.model.ts$/
+                }
+            }
+        })
     ],
     define: {
         __VERSION__: `"${pkg.version}"`,
